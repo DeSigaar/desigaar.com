88f877df971ef93ade1f0248a5a154a6026aa4e6
MaxAltena <Max@MaxAltena.com>
2019-05-31 21:28:29 +0200

    Update deploy.js

--- .circleci/deploy.js
+++ .circleci/deploy.js
@@ -6,7 +6,7 @@ var config = {
   password: process.env.FTPPASS,
   host: process.env.FTPHOST,
   port: 21,
-  localRoot: __dirname + "/../public",
+  localRoot: __dirname + "/public",
   remoteRoot: process.env.FTPPATH,
   deleteRemote: true,
   include: ["*"]
